ff38781e788f45a1b8354d7343d2f8a20a984ac5
Lena Harris <kayoriayane@Lenas-MacBook-Pro.local>
2020-09-11 12:56:41 -0500

    Utterly and completely failed everything.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Characters from './components/Character.js'
+import Character from './components/Character.js'
 import './App.css';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+	// Try to think through what state you'll need for this app before starting. Then build out the state properties here.
+	const [characters, setCharacters ] = useState();
+	
+	// Fetch characters from the API in an effect hook. Remember, anytime you have a side effect in a component, you want to think about which state and/or props it should sync up with, if any.
+	useEffect(() => {
+		axios.get('https://swapi.dev/api/people/')
+		.then(result => {
+			setCharacters(result.data.results);
+		})
+		.catch(err => console.log(err));
+	}, []);
+	console.log(characters[1]);
+	
+	return (
+		<div className='App'>
+			<h1 className='Header'>Characters</h1>
+			<Characters list='{characters}' />
+			<Character character='{id}' />
+		</div>
+	);
 }
 
 export default App;
